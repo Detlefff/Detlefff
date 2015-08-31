4ebafec92a93b1b145e0c6232adc58ae9ac5938a
Jannick Fahlbusch <git@jf-projects.de>
2015-08-31 19:57:01 +0200

    Loop through an optional RegEx-Array

--- classes/events.class.php
+++ classes/events.class.php
@@ -95,28 +95,42 @@ class MyEvents extends AllEvents
 		//directories within the scripts/ dir and search for valid regexes there!
 		require './config/regEx.php';
 
-		$message = new Message($mynumber, $from, $id, $type, $time, $name, $body);
+		$this->message = new Message($mynumber, $from, $id, $type, $time, $name, $body);
 
 		echo "$name ($from) is writing: \n $body \n";
 
 		$this->whatsProt->sendMessageComposing(split('@', $from)[0]);
 
 		foreach ($regex as $key => $value) {
-			if(preg_match($value, $body, $matches)) {
-
-				$script = new Script($message, $matches, $this->whatsProt);
-
-				require_once 'scripts/' . $key . '/' . $key . '.php';
-
-				$script = new $key ($message, $matches, $this->whatsProt);
-
-				$script->run();
-
-				$script->__destruct();
-				break;
+			if(is_array($value)) {
+				foreach ($value as $pattern) {
+					if(preg_match($pattern, $body, $matches)) {
+						$this->execute($matches, $key);
+						break;
+					}
+				}
+			} else {
+				if(preg_match($value, $body, $matches)) {
+					$this->execute($matches, $key);
+					break;
+				}
 			}
 		}
 
-		$this->whatsProt->sendMessagePaused($message->number);
+		$this->whatsProt->sendMessagePaused($this->message->number);
+	}
+
+	private function execute($matches, $name)
+	{
+		$script = new Script($this->message, $matches, $this->whatsProt);
+
+		require_once 'scripts/' . $name . '/' . $name . '.php';
+
+		$plugin = new $name ($this->message, $matches, $this->whatsProt);
+
+		$plugin->run();
+
+		$plugin->__destruct();
+		$script->__destruct();
 	}
 }
